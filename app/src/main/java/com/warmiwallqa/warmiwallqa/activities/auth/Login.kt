01279a2b38cf7031db72c85e7f6d5a96a7d7424c
diegozen21 <diego.razen@gmail.com>
2024-03-11 10:51:05 -0500

    declaracion de variables

--- app/src/main/java/com/warmiwallqa/warmiwallqa/activities/auth/Login.kt
+++ app/src/main/java/com/warmiwallqa/warmiwallqa/activities/auth/Login.kt
@@ -2,14 +2,20 @@ package com.warmiwallqa.warmiwallqa.activities.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import com.warmiwallqa.warmiwallqa.R
 
 class Login : AppCompatActivity() {
+
+    private lateinit var textUsuario: EditText
+    private lateinit var textPassword: EditText
+    private lateinit var loginButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
 
-        print("TEXTO")
 
     }
 }
