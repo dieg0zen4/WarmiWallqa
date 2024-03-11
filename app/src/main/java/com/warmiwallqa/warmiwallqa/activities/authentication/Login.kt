21c0000ab067f3478cd250ab84133c7d83e8acef
diegozen21 <diego.razen@gmail.com>
2024-03-11 10:38:51 -0500

    se creó la interfas del login

---
+++ app/src/main/java/com/warmiwallqa/warmiwallqa/activities/authentication/Login.kt
@@ -0,0 +1,15 @@
+package com.warmiwallqa.warmiwallqa.activities.authentication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.warmiwallqa.warmiwallqa.R
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login)
+
+        print("TEXTO")
+
+    }
+}
