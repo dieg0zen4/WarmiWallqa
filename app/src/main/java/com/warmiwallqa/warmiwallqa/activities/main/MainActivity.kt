90417f35938f5b78817e6e9455eb6fc788e7aa93
diegozen21 <diego.razen@gmail.com>
2024-03-11 10:39:37 -0500

    se agregaron fragments y activities por defecto

--- app/src/main/java/com/warmiwallqa/warmiwallqa/activities/main/MainActivity.kt
+++ app/src/main/java/com/warmiwallqa/warmiwallqa/activities/main/MainActivity.kt
@@ -1,14 +1,12 @@
-package com.warmiwallqa.warmiwallqa
+package com.warmiwallqa.warmiwallqa.activities.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.warmiwallqa.warmiwallqa.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        print("TEXTO")
-
     }
 }
